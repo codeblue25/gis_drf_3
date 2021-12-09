bc679ecacb66ff6816e4e7456a0408978af85452
codeblue25 <chois1031@gmail.com>
2021-12-09 13:55:23 +0900

    Create hello_world_template views

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, hello_world_template
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    # UI를 보여주는 views 라우팅
+    path('hello_world_template/', hello_world_template, name='hello_world_template'),
+    # 로직 처리하는 views 라우팅
     path('hello_world/', hello_world, name='hello_world'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+# 템플릿(UI) 처리 view
+def hello_world_template(request):
+    return render(request, 'accountapp/hello_world.html')
+
+
+# 로직 처리 view
 @api_view()
 def hello_world(request):
     return Response({'message': 'Hello world !'})
