3127b22949d3169ee1989d6e901cbb5cf9851a99
codeblue25 <chois1031@gmail.com>
2021-12-09 15:10:53 +0900

    Insert axios async http get method

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,4 +13,4 @@ def hello_world_template(request):
 # 로직 처리 view
 @api_view()
 def hello_world(request):
-    return Response({'message': 'Hello world !'})
+    return Response({'message': 'Hello JS !'})
