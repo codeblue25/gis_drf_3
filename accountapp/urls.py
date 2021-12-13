e576f680193c75cda623a03a3ff019f1c8e1ba0f
codeblue25 <chois1031@gmail.com>
2021-12-13 15:56:54 +0900

    Create logout Views

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, AccountLoginView
@@ -11,9 +12,10 @@ urlpatterns = [
     # 로직 처리하는 views 라우팅
     path('hello_world/', hello_world, name='hello_world'),
 
-    # 로그인 views 라우팅
+    # 로그인, 로그아웃 views 라우팅
     path('login_template/', AccountLoginView, name='login_template'),
     path('login/', views.obtain_auth_token, name='login'),
+    path('logout_template/', TemplateView.as_view(template_name='accountapp/logout.html'), name='logout_template'),
 
     # 계정 생성하는 views 라우팅
     path('create_template/', AccountCreateTemplate, name='create_template'),
