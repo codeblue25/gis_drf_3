b6464dcdbba40706245c3f83f8fab94531c21dd9
codeblue25 <chois1031@gmail.com>
2021-12-13 10:04:11 +0900

    Update DRF create javascript

--- accountapp/static/JavaScript/create.js
+++ accountapp/static/JavaScript/create.js
@@ -1,22 +1,21 @@
 
 function send_input() {
-    axios.post('/accounts/hello_world/', {
-        input_data: document.getElementById('input_data').value,
-      })
+    axios.post('/accounts/create/', {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+    })
       .then(function (response) {
         // handle success
         console.log(response);
-        // POST 요청이 성공했을 때 하는 JS 작업: Hello JS!
-        document.getElementById('text').innerHTML = response.data['text'];
-        document.getElementById('new_model_created_at').innerHTML = response.data['created_at'];
-        // 실시간으로 시각화
-        document.getElementById('new_model_list').innerHTML
-                 += '<h5>' + response.data['text'] + '</h5>'
-        document.getElementById('new_model_list').innerHTML
-                 += '<p>' + response.data['created_at'] + '</p>'
 
+        document.getElementById('alert_box').innerHTML
+                 += "<div class='btn btn-primary rounded-pill px-5 mb-3'> 가입을 성공했습니다. </div>"
       })
       .catch(function (error) {
+      // handle failure
         console.log(error);
+
+        document.getElementById('alert_box').innerHTML
+                 += "<div class='btn btn-danger rounded-pill px-5 mb-3'> 가입을 실패했습니다. </div>"
       });
 }
