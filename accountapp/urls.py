4e534958065bc46885b6e7ec5573be3212c4ce39
codeblue25 <chois1031@gmail.com>
2021-12-14 16:37:26 +0900

    Refactor REST architecture

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,8 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView, \
-    AccountLoginView, AccountRetrieveAPIView, AccountRetrieveTemplateView, AccountUpdateAPIView, \
-    AccountUpdateTemplateView, AccountDestroyAPIView, AccountDestroyTemplateView
+    AccountLoginView, AccountRetrieveTemplateView, AccountUpdateTemplateView, AccountDestroyTemplateView, AccountRUDAPIView
 
 app_name = 'accountapp'
 
@@ -21,18 +20,23 @@ urlpatterns = [
 
     # 계정 생성하는 views 라우팅
     path('create_template/', AccountCreateTemplate, name='create_template'),
-    path('create/', AccountCreateAPIView.as_view(), name='create'),
+    path('', AccountCreateAPIView.as_view(), name='create'),
 
-    # 유저 조회하는 views 라우팅
+    # 유저 정보 조회, 업데이트, 삭제하는 template 라우팅
     path('retrieve_template/<int:pk>', AccountRetrieveTemplateView.as_view(), name='retrieve_template'),
-    path('retrieve/<int:pk>', AccountRetrieveAPIView.as_view(), name='retrieve'),
-
-    # 유저 정보 업데이트 하는 views 라우팅
     path('update_template/<int:pk>', AccountUpdateTemplateView.as_view(), name='update_template'),
-    path('update/<int:pk>', AccountUpdateAPIView.as_view(), name='update'),
-
-    # 유저 정보 삭제하는 views 라우팅
-
     path('delete_template/<int:pk>', AccountDestroyTemplateView.as_view(), name='delete_template'),
-    path('delete/<int:pk>', AccountDestroyAPIView.as_view(), name='delete'),
+    # 유저 정보 조회, 업데이트, 삭제하는 Views 라우팅
+    path('<int:pk>', AccountRUDAPIView.as_view(), name='RUD')
 ]
+
+"""
+ListCreateAPIView
+127.0.0.1:9000/accounts  + HTTP method + GET = LIST  => 지금 당장 필요 없음
+127.0.0.1:9000/accounts  + HTTP method + POST = Create
+
+RetrieveUpdateDestroyAPIView
+127.0.0.1:9000/accounts/1  + HTTP method + GET = Detail
+127.0.0.1:9000/accounts/1  + HTTP method + PUT / PATCH  = Update
+127.0.0.1:9000/accounts/1  + HTTP method + DELETE = Delete
+"""
