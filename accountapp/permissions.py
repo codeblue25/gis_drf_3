65b55fe314751897b772aca76445c76d911e1ab7
codeblue25 <chois1031@gmail.com>
2021-12-14 16:53:32 +0900

    Update isOwner permission from RUD

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -1,6 +1,10 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj == request.user
