62daadb910593240a4e99a2ef49ea50382a6a5f6
codeblue25 <chois1031@gmail.com>
2021-12-15 09:31:20 +0900

    Update javascript urls

--- accountapp/static/JavaScript/destroy.js
+++ accountapp/static/JavaScript/destroy.js
@@ -1,7 +1,8 @@
+
 function delete_account(pk) {
     axios({
         method: 'delete',
-        url: '/accounts/delete/' + pk,
+        url: '/accounts/' + pk,
         headers: {
             Authorization: decodeURIComponent(getCookie('drf_token')),
         }
@@ -10,7 +11,7 @@ function delete_account(pk) {
         // handle success
         console.log(response);
 
-        window.location.href = '/accounts/hello_world_template/';
+        window.location.href = '/accounts/logout_template/';
       })
       .catch(function (error) {
         // handle error
