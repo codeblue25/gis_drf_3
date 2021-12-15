790f519ce31b7acc22301030948bf72184d03ecc
codeblue25 <chois1031@gmail.com>
2021-12-15 15:54:34 +0900

    Update image upload of DRF Profile Create API template with javascript

---
+++ profileapp/static/profileapp/JavaScript/create.js
@@ -0,0 +1,33 @@
+
+function send_input() {
+    var form = new FormData();
+    form.append('nickname', document.getElementById('nickname').value);
+    form.append('image', document.getElementById('image').files[0]);
+    form.append('message', document.getElementById('message').value);
+
+    axios({
+        method: 'post',
+        url: '/profiles/',
+        data: form,
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+      })
+
+      .then(function (response) {
+        // handle success
+        console.log(response);
+
+        // redirect success_url
+        // window.location.href = '';
+        document.getElementById('alert_box').innerHTML
+                 += "<div class='btn btn-primary rounded-pill px-5 mb-3'> 프로필 생성을 성공했습니다</div>"
+      })
+      .catch(function (error) {
+      // handle failure
+        console.log(error);
+
+        document.getElementById('alert_box').innerHTML
+                 += "<div class='btn btn-danger rounded-pill px-5 mb-3'> 프로필 생성을 실패했습니다</div>"
+      });
+}
