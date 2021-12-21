ec076dfba9f558ff2ae90d640e0f81b4e9bd9f03
codeblue25 <chois1031@gmail.com>
2021-12-21 17:32:43 +0900

    Update Accountapp nav btn

--- accountapp/static/accountapp/JavaScript/token_check.js
+++ accountapp/static/accountapp/JavaScript/token_check.js
@@ -9,12 +9,26 @@ function getCookie(name) {
 var check = getCookie('drf_token');
 
 if (check !== undefined) {
-    document.getElementById('signup_btn').innerHTML =
-        "<a href=\"/accounts/retrieve_template/\">\n" +
-        "                Mypage\n" +
-        "            </a>";
     document.getElementById('login_btn').innerHTML =
         "<a href=\"/accounts/logout_template/\">\n" +
         "                Logout\n" +
         "            </a>";
+
+    var user_id;
+
+    axios({
+        method: 'get',
+        url: '/accounts/token/',
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
+    .then(function (response) {
+        // handle success
+        user_id = response.data['id'];
+        document.getElementById('signup_btn').innerHTML =
+        "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
+        "                MyPage\n" +
+        "            </a>";
+    })
 }
