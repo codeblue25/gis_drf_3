45614c2dbc9b67675f1185c28a99a89ded2b3b01
codeblue25 <chois1031@gmail.com>
2021-12-21 15:50:29 +0900

    Update Profileapp initialize function

--- profileapp/static/profileapp/JavaScript/update.js
+++ profileapp/static/profileapp/JavaScript/update.js
@@ -1,13 +1,25 @@
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 function initialize(pk) {
-    axios.get('/profiles/' + pk)
+    axios({
+        method: 'get',
+        url: '/profiles/' + pk,
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
       .then(function (response) {
         // handle success
         console.log(response);
 
         document.getElementById('nickname').value = response.data['nickname'];
         document.getElementById('message').value = response.data['message'];
-//        document.getElementById('image').value = response.data['image'];
 
       })
       .catch(function (error) {
