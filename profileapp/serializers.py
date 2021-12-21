c3df95ae26b3bdb223be5948c442dc31b5b921f4
codeblue25 <chois1031@gmail.com>
2021-12-21 23:59:05 +0900

    Update DRF Profileapp thumbnail generation

--- profileapp/serializers.py
+++ profileapp/serializers.py
@@ -5,4 +5,4 @@ from profileapp.models import Profile
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'nickname', 'image', 'message', 'owner_id']
+        fields = ['id', 'nickname', 'image', 'thumb', 'message', 'owner_id']
