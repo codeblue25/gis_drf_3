ad6c7d692f056f5092e91320eea1859bd5698012
codeblue25 <chois1031@gmail.com>
2021-12-21 14:47:28 +0900

    Update javascript path

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from profileapp.models import Profile
@@ -30,7 +30,7 @@ class ProfileUpdateTemplateView(TemplateView):
     template_name = 'profileapp/update.html'
 
 
-class ProfileUpdateAPIView(UpdateView):
+class ProfileUpdateAPIView(UpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsProfileOwner]
